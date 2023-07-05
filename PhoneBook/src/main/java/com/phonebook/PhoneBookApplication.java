bb75eb2463f3d6b638369e7e40700b51f0e329f7
Omkar Vyavahare <omkarbv2000@gmail.com>
2023-07-06 00:04:14 +0530

    updated

--- PhoneBook/src/main/java/com/phonebook/PhoneBookApplication.java
+++ PhoneBook/src/main/java/com/phonebook/PhoneBookApplication.java
@@ -1,5 +1,6 @@
 package com.phonebook;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PhoneBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhoneBookApplication.class, args);
+		String s="qwe";
+		System.out.println(s.charAt(12));
+		
+		//SpringApplication.run(PhoneBookApplication.class, args);
 	}
-
 }
